f014b3b2de793b52b014f6f78d508e9f85568694
Julien Delafontaine <julien.delafontaine@epfl.ch>
2011-09-27 11:43:20 +0200

    correction 4.1

--- week2/Chapter2_solutionB.r
+++ week2/Chapter2_solutionB.r
@@ -1,5 +1,16 @@
-data = read.table("annot.txt", header=TRUE, sep='\t', dec='.')
-colnames(data)
-start = data['txStart']
-end = data['txEnd']
-sizes = end-start
+
+## Exercise 4.1
+gtf <- read.table('chr18.gtf',header=TRUE,sep='\t') # If you forget the "sep" argument, it cuts all spaces.
+exon.lines <- gtf["feature"]=='exon' # The indices of all lines of type "exon".
+gtf <- gtf[exon.lines,] # The "exon" type lines of the original table.
+colnames(gtf) # You see that you need "start" and "end".
+starts = gtf["start"]
+ends = gtf["end"]
+lengths = ends-starts
+gtf["exonSize"] <- lengths # Adds a new column to the table.
+hist(lengths[,1])
+
+## Exercise 4.2
+
+
+
