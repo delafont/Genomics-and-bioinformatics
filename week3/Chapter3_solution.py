62e753dadf047d9e2a9cb258881ad9b145fffd17
Lucas Sinclair <lucas.sinclair@me.com>
2011-09-29 14:33:45 +0200

    edges_matrix

--- week3/Chapter3_solution.py
+++ week3/Chapter3_solution.py
@@ -9,14 +9,15 @@ def overlaps(read1, read2, min_overlap=10):
     """Returns a positive number if the end of read1
        overlaps with the begining of read2, 0 otherwise"""
     for i in xrange(read_length, min_overlap, -1):
-        if read1[-i:] == read2[0:i]: return i
-    return 0.0
+        if read1[-i:] == read2[0:i]: return read1[-i:]
+    return False
 
 # Question 2.3 #
-from numpy import zeros
-edges_matrix = zeros((num_of_reads, num_of_reads))
+edges_matrix = []
 for i in xrange(num_of_reads):
+    edges_matrix.append([])
     for j in xrange(num_of_reads):
+        edges_matrix[i].append(False)
         if i == j: continue
         edges_matrix[i][j] = overlaps(reads[i], reads[j])
 
