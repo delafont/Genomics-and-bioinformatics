5f9d6f3cd7a688f6ed229ab4926db9f9a8846f70
Lucas Sinclair <lucas.sinclair@me.com>
2011-09-29 13:51:26 +0200

    Continuing the week3 exercise

--- week3/Chapter3_solution.py
+++ week3/Chapter3_solution.py
@@ -1,18 +1,23 @@
 # Question 2.1 #
 with open('reads.fastq', 'r') as f:
-    v = [line.strip() for line in f]
-num_of_reads = len(v)
-read_length = len(v[1])
+    reads = [line.strip() for line in f]
+num_of_reads = len(reads)
+read_length = len(reads[1])
 
 # Question 2.2 #
 def overlaps(read1, read2, min_overlap=10):
-    """Returns True if end of read1 overlaps with
-       the begining of read2, False otherwise"""
-    for i in xrange(min_overlap, read_length):
-        if read1[-i] == read2[i]: return True
-    return False
+    """Returns a positive number if the end of read1
+       overlaps with the begining of read2, 0 otherwise"""
+    for i in xrange(read_length, min_overlap, -1):
+        if read1[-i:] == read2[0:i]: return i
+    return 0.0
 
 # Question 2.3 #
 from numpy import zeros
 edges_matrix = zeros((num_of_reads, num_of_reads))
-for i,j in zip(xrange(num_of_reads), file2)
+for i in xrange(num_of_reads):
+    for j in xrange(num_of_reads):
+        if i == j: continue
+        edges_matrix[i][j] = overlaps(reads[i], reads[j])
+
+# Question 2.4 #
