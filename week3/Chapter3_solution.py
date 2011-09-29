c8c6a731a146bf3962d7fa35e270c76ff1ec1385
Lucas Sinclair <lucas.sinclair@me.com>
2011-09-29 15:28:30 +0200

    making v and e

--- week3/Chapter3_solution.py
+++ week3/Chapter3_solution.py
@@ -10,15 +10,10 @@ def overlaps(read1, read2, min_overlap=10):
        overlaps with the begining of read2, 0 otherwise"""
     for i in xrange(read_length, min_overlap, -1):
         if read1[-i:] == read2[0:i]: return read1[-i:]
-    return False
+    return 0
 
 # Question 2.3 #
-edges_matrix = []
-for i in xrange(num_of_reads):
-    edges_matrix.append([])
-    for j in xrange(num_of_reads):
-        edges_matrix[i].append(False)
-        if i == j: continue
-        edges_matrix[i][j] = overlaps(reads[i], reads[j])
+v = reads
+e = [(r1,r2) for r1 in l for r2 in l if r1 != r2 and overlaps(r1,r2)]
 
 # Question 2.4 #
