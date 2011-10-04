d8a061d2da41ccc97b0e94dc98a79105e34ad031
Julien Delafontaine <julien.delafontaine@epfl.ch>
2011-10-04 23:32:50 +0200

    asserts

--- week3/findcycles.py
+++ week3/findcycles.py
@@ -51,50 +51,44 @@ E = [("AA","AB"),("AB","BC"),("BC","CD"),("CD","DE"),("DE","EF"),("EF","AA"),("A
 r = ['AA', 'AB', 'EF', 'CD', 'AB', 'BC', 'CD', 'DE', 'EF', 'AA']
 """
 
-def adjacents(vertex, edges):
-    """Returns the list of edges from *edges* adjacent to node *vertex*."""
+def outgoing(vertex, edges):
+    """Returns the list of edges from *edges* entering into node *vertex*."""
     return [edge for edge in edges if edge[0] == vertex]
+
+def incoming(vertex, edges):
+    """Returns the list of edges from *edges* coming from node *vertex*."""
+    return [edge for edge in edges if edge[1] == vertex]
 
 def walk(vertex, edges):
     """From node *vertex*, walk along edges *edges*, never taking an already
     used one. Return the chosen *path* and the remaining edges.
     If the graph is Eulerian, *path* is a cycle."""
-    path = [vertex]; adj = adjacents(vertex,edges)
+    path = [vertex]; adj = outgoing(vertex,edges)
     while adj:
         e = adj[0]
         edges.remove(e)
         path.append(e[1])
         vertex = e[1]
-        adj = adjacents(vertex,edges)
+        adj = outgoing(vertex,edges)
     return path, edges
 
 def hierholzer(vertices, edges):
     """Finds an Eulerian cycle in a connected Eulerian graph defined
     by the set *vertices* of its vertices and the set *edges* of its edges.
     The cycle is returned as a list of vertices."""
-    assert any([len(adjacents(v,edges))%2==1 for v in vertices]), "Your graph is not Eulerian."
+    err = "Your graph is either not Eulerian, or connected, or cyclic."
+    assert all([(len(outgoing(v,edges))+len(incoming(v,edges)))%2==0 for v in vertices]), err
     v = vertices[0]
     cycle, edges = walk(v, edges)
+    assert cycle[0] == cycle[-1], err
     notvisited = set(cycle)
     while len(notvisited) != 0:
         v = notvisited.pop()
-        if len(adjacents(v, edges)) != 0:
+        if len(outgoing(v, edges)) != 0:
             i = cycle.index(v)
             sub, E = walk(v, edges)
+            assert sub[0] == sub[-1], err
             cycle = cycle[:i]+sub+cycle[i+1:]
             notvisited.update(sub)
     return cycle[:-1]
 
-"""
-EXAMPLE
-path = ['AATGT', 'ATGTC', 'GTCGA', 'CGATT']
-return = AATGTCGATT
-"""
-# Sequence retrival #
-def make_sequence(path, min_overlap):
-    """From a list of vertices, builds the contig"""
-    sequence = path[0]
-    for i, vertex in enumerate(path[1:]):
-        sequence = overlaps(sequence, vertex, min_overlap)
-        if not sequence: raise Exception("You didn't input a path: " + path[i-1] + '-' + path[i] + '-' + path[i+1] )
-    return sequence
