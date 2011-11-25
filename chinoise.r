aa200a2842c44fc200ef88f92253f4e76a8a1a3c
Julien Delafontaine <julien.delafontaine@epfl.ch>
2011-11-25 15:05:09 +0100

    fixe test pour la chinoise

--- chinoise.r
+++ chinoise.r
@@ -15,7 +15,8 @@ multest <- function(c1,c2){
     B = L1(c1,c2)
     LR = A/B
     lr = -2*log(LR)
-    pchisq(lr,length(c1))
+    pvalue = 1-pchisq(lr,length(c1))
+    LR
 }
 
 multest(c(17,4,1,0),c(6,7,4,1)) # P7-P10
