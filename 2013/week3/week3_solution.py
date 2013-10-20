8776e1f4687c1b45952fc3dc0a52890b4513beac
Julien Delafontaine <julien.delafontaine@epfl.ch>
2013-10-20 14:22:43 +0200

    No dependance to biopython, and better consistency

--- 2013/week3/week3_solution.py
+++ 2013/week3/week3_solution.py
@@ -4,12 +4,13 @@
 
 from Bio import SeqIO
 genome = SeqIO.read("chrI.fa", "fasta")  # This creates a 'SeqRecord' object
+sequence = genome.seq  # a 'Seq' object, similar to a string
 
 """
 Alternatively:
 
 f = open("chrI.fa")
-genome = f.read()
+sequence = f.read()
 f.close()
 
 Note that in this case all the file is read as a single string.
@@ -21,59 +22,65 @@ Note also the multiple-line comment we use here.
 # Question 3.2 #
 #--------------#
 
-total_length = len(genome.seq)  # genome.seq is a 'Seq' object, similar to a string
+total_length = len(sequence)
 print "The length is:", total_length
-
-"""
-Alternatively:
-
-total_length = len(genome)
-print "The length is:", total_length
-"""
 
 #--------------#
 # Question 3.3 #
 #--------------#
 
-A_count = genome.seq.count('A') + genome.seq.count('a')
-C_count = genome.seq.count('C') + genome.seq.count('c')
-G_count = genome.seq.count('G') + genome.seq.count('g')
-T_count = genome.seq.count('T') + genome.seq.count('t')
-print 'A: %s, C: %s, G: %s, T: %s' % (A_count, C_count, G_count, T_count)
+sequence = sequence.lower()  # transforms characters to lower case
+a = sequence.count('a')
+c = sequence.count('c')
+g = sequence.count('g')
+t = sequence.count('t')
+print 'A: %s, C: %s, G: %s, T: %s' % (a,c,g,t)
 # each '%s' will be replaced by the corresponding string on the right of the '%'
 
-"""
-Alternatively:
+# Alternatively:
 a=0; c=0; g=0; t=0; n=0
-for base in genome:
-    base = base.lower() # lower() transforms characters to lower case
+for base in sequence:
+    base = base.lower()
     if base == 'a': a+=1 # equivalent to a = a+1
     elif base == 'c': c+=1
     elif base == 'g': g+=1
     elif base == 't': t+=1
     else: n+=1 # There are 'N's in the file to indicate repeated (uninteresting) sequences.
-"""
+print 'A: %s, C: %s, G: %s, T: %s' % (a,c,g,t)
 
 #--------------#
 # Question 3.4 #
 #--------------#
 
+# Either import the Biopyton GC function:
 from Bio.SeqUtils import GC
-gc_percentage = GC(genome.seq)
+
+# or use your own:
+def GC(sequence):
+    a=0; c=0; g=0; t=0; n=0
+    for base in sequence:
+        base = base.lower()
+        if base == 'a': a+=1
+        elif base == 'c': c+=1
+        elif base == 'g': g+=1
+        elif base == 't': t+=1
+        else: n+=1
+    return 100*float(c+g)/len(sequence)
+
+gc_percentage = GC(sequence)
 print "The total GC percentage is:", gc_percentage
 
 #---------------------------#
 # Question 3.5(a) computing #
 #---------------------------#
 
-number_of_windows = 1000 # Arbitrary
-window_size = total_length / number_of_windows
+window_size = 100000  # arbitrary
 start_positions = range(0, total_length, window_size)
 gc_per_window = []
 
 for start in start_positions:
     stop  = start + window_size
-    gc_in_window = GC(genome[start:stop].seq)
+    gc_in_window = GC(sequence[start:stop])
     gc_per_window.append(gc_in_window)
 
 #--------------------------#
